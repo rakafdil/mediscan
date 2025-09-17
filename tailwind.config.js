85a3f6894ece7ccae82667cfbaef1e2dffe91392
VeryFach <162655143+VeryFach@users.noreply.github.com>
2025-09-17 21:16:11 +0700

    refactor: Remove Playwright configuration and related test files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: "class",
     content: [
         "./src/**/*.{js,ts,jsx,tsx}",
         "./src/app/**/*.{js,ts,jsx,tsx}",
